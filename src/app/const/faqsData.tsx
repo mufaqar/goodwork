5f34881d1a84c9c64600d4ad023793c88855f7e3
mufaqar <mufaqar@gmail.com>
2023-06-19 10:48:26 +0500

    FAQS

--- src/app/const/faqsData.tsx
+++ src/app/const/faqsData.tsx
@@ -6,7 +6,7 @@ export const Faqs_Data = [
     },
     {
         id: 2,
-        question: "We are Exchange hosting provider. Many customers want to sync user's password from their Active Directory to our Active Directory",
+        question: "We are Exchange hosting provider. Many customers want to sync user's password from their Active Directory to our Active Directory ?",
         answer: 'Simply Sync Password tool enables password sync between different forests in Active Directory without the need to open any ports on your firewall. Simply Sync Password also doesn’t require any trust relationship between forests.'
     },
     {
@@ -16,7 +16,7 @@ export const Faqs_Data = [
     },
     {
         id: 4,
-        question: 'I want to route Exchange mail based on sender domain',
+        question: 'I want to route Exchange mail based on sender domain ?',
         answer: 'Simply Sync Password tool enables password sync between different forests in Active Directory without the need to open any ports on your firewall. Simply Sync Password also doesn’t require any trust relationship between forests.'
     },
 ]
