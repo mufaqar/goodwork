bf71179b96e7479cac40c444f1cf1b28c279f45a
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-06-21 20:07:17 +0500

    updated

--- src/app/components/qa-tabs.tsx
+++ src/app/components/qa-tabs.tsx
@@ -33,7 +33,7 @@ const QA_Tabs = () => {
                 <div className='md:w-3/4 w-[65%]'>
                     <div className='flex gap-1 justify-between mb-5'>
                         <QA_Filters />
-                        <button className='bg-darkGreen md:px-[30px] px-1 md:py-[10px] py-0 rounded-[40px] md:text-lg text-xs font-medium text-white'>
+                        <button className='bg-darkGreen hover:bg-white md:px-[30px] px-1 md:py-[10px] py-0 rounded-[40px] md:text-lg text-xs font-medium text-white hover:text-darkGreen border border-darkGreen'>
                             Create a New Thread
                         </button>
                     </div>
@@ -148,7 +148,7 @@ const QA_Tabs = () => {
 
 export default QA_Tabs
 
-export const QA_Filters = ({ filter, name, values }: any) => {
+export const QA_Filters = () => {
     return (
         <div className='max-w-[91px]'>
             <label htmlFor="que_ans" className="md:text-lg text-xs font-normal text-darkBlue hidden">Latest</label>
