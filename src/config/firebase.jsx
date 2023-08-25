185bc1ba24fdc6fd1cacb11e8fcbe8fea52d990c
mufaqar <mufaqar@gmail.com>
2023-08-25 20:15:55 +0500

    update firebase

--- src/config/firebase.jsx
+++ src/config/firebase.jsx
@@ -1,17 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDAB8nExNRWktdi2VNkOBzBVbLxNYyxxPk",
-  authDomain: "igoodwork-efed3.firebaseapp.com",
-  projectId: "igoodwork-efed3",
-  storageBucket: "igoodwork-efed3.appspot.com",
-  messagingSenderId: "921697898225",
-  appId: "1:921697898225:web:6b9998b883974aa5176350"
+ apiKey: "AIzaSyDVcRHicQY3tyEX3vf6qrpU4Pk8oe85GMg",
+  authDomain: "igoodworksweb.firebaseapp.com",
+  projectId: "igoodworksweb",
+  storageBucket: "igoodworksweb.appspot.com",
+  messagingSenderId: "1036191184084",
+  appId: "1:1036191184084:web:bb75195883ea770e16b741",
+  measurementId: "G-M7WRCYBKRL"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth  = getAuth(app)
