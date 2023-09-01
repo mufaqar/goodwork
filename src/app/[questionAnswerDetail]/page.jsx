54002ee6345f53f0479e8e02c71bd734042a4933
syed zunair gillani <gillanix007@gmail.com>
2023-09-01 16:22:27 +0500

    update

--- src/app/[questionAnswerDetail]/page.jsx
+++ src/app/[questionAnswerDetail]/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import Page_Banner from '../components/page-banner';
@@ -8,34 +8,21 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Author from '../../../public/images/user.png';
 import Support from '../../../public/images/support.png';
-import { client } from '../../../sanity/lib/client';
-import { useParams } from 'next/navigation'
-import { QSingleFaq } from '../../../sanity/lib/queries';
 import { dateFormate } from '@/lib';
 import BlockContent from "@sanity/block-content-to-react";
 import { projectId } from '../../../sanity/env';
+import { SettingsContext } from '@/context/setting-context';
 
 
 
 
 
-const Question_Answer_Detail = async () => {
-    const params = useParams()
-   
-    const [detail, setDetail] = useState()    
-    console.log("🚀 ~ file: page.jsx:23 ~ constQuestion_Answer_Detail= ~ detail:", detail)
-
-    useEffect(() => {  
-        const slug = params?.questionAnswerDetail 
-        const fetch = async () => {
-            const res = await client.fetch(QSingleFaq, { slug })
-            setDetail(res)
-        }
-        fetch()
-    },[detail])
+const Question_Answer_QAdetail = () => {
+    const {QAdetail} = useContext(SettingsContext)
+    console.log("🚀 ~ file: page.jsx:22 ~ constQuestion_Answer_QAdetail= ~ QAdetail:", QAdetail)
 
     return (
-        <main>
+        <div>
             <Header />
             <Page_Banner
                 title="Q&A"
@@ -51,14 +38,14 @@ const Question_Answer_Detail = async () => {
                     <div className='bg-[#F6FAFE] md:p-12 p-5 rounded-[28px] max-w-[1042px]'>
                         <div className='flex justify-between'>
                             <h2 className='md:text-2xl text-base font-medium text-darkBlue'>
-                                {detail?.question}
+                                {QAdetail?.question}
                             </h2>
                             <div className='flex gap-2 items-center'>
                                 <Image src={Author} alt="uset" />
                                 <p className='grid'>
                                     <span className='md:text-lg text-base font-medium text-darkBlue'>
-                                        {detail?.createdby} <span className='text-sm font-medium text-darkBlue/50'>
-                                            . {dateFormate(detail?._createdAt)}
+                                        {QAdetail?.createdby} <span className='text-sm font-medium text-darkBlue/50'>
+                                            . {dateFormate(QAdetail?._createdAt)}
                                         </span>
                                     </span>
                                     <span className='text-sm font-medium text-darkBlue/50'>
@@ -69,7 +56,7 @@ const Question_Answer_Detail = async () => {
                         </div>
                         <div className='h-[1px] bg-darkBlue/10 my-5'></div>
                         <BlockContent
-                        blocks={detail?.answer}
+                        blocks={QAdetail?.answer}
                         projectId={projectId}
                         dataset="production"
                       />
@@ -77,14 +64,14 @@ const Question_Answer_Detail = async () => {
                     <div className='bg-[#F3F3F3] md:p-12 p-5 rounded-[28px] max-w-[1042px] md:ml-auto mt-7'>
                         <div className='flex justify-between'>
                             <h2 className='md:text-2xl text-base font-medium text-darkBlue'>
-                                re: {detail?.question}
+                                re: {QAdetail?.question}
                             </h2>
                             <div className='flex gap-2 items-center'>
                                 <Image src={Support} alt="uset" />
                                 <p className='grid'>
                                     <span className='md:text-lg text-base font-medium text-darkBlue'>
                                         Support <span className='text-sm font-medium text-darkBlue/50'>
-                                            . {dateFormate(detail?._createdAt)}
+                                            . {dateFormate(QAdetail?._createdAt)}
                                         </span>
                                     </span>
                                     <span className='text-sm font-medium text-darkBlue/50'>
@@ -96,7 +83,7 @@ const Question_Answer_Detail = async () => {
                         <div className='h-[1px] bg-darkBlue/10 my-5'></div>
                         <div className='text-lg font-normal text-darkBlue mb-3'>
                             <BlockContent
-                            blocks={detail?.issue}
+                            blocks={QAdetail?.issue}
                             projectId={projectId}
                             dataset="production"
                           />
@@ -106,8 +93,8 @@ const Question_Answer_Detail = async () => {
                 </div>
             </section>
             <Footer />
-        </main>
+        </div>
     )
 }
 
-export default Question_Answer_Detail
+export default Question_Answer_QAdetail
--- src/context/setting-context.jsx
+++ src/context/setting-context.jsx
@@ -2,7 +2,10 @@
 'use client'
 
 
+import { useParams } from "next/navigation";
 import { createContext, useEffect, useState } from "react";
+import { client } from "../../sanity/lib/client";
+import { QSingleFaq } from "../../sanity/lib/queries";
 
 
 export const SettingsContext = createContext();
@@ -10,20 +13,33 @@ export const SettingsContext = createContext();
 export const SettingsProvider = ({ children }) => {
 
   const [lsuser, lsSetUser] = useState(null)
+  const params = useParams()
+  const [QAdetail, setQADetail] = useState()  
 
   useEffect(()=>{
     const user = JSON.parse(localStorage.getItem('user'))
     lsSetUser(user)
+    
+    const slug = params?.questionAnswerDetail 
+    if(slug){
+        const fetch = async () => {
+            const res = await client.fetch(QSingleFaq, { slug })
+            setQADetail(res)
+        }
+        fetch()
+    }
   },[])
   
-  
+    
+
+   
 
 
 
   return (
     <SettingsContext.Provider
       value={{
-         lsuser, lsSetUser
+         lsuser, lsSetUser, QAdetail, setQADetail
       }}
     >
       {children}
